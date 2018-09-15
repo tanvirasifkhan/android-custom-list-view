19d5826fd3c5fa4c565ea95ef3a1b2bd31cc7d72
Asif Khan <asifkhan.github@gmail.com>
2018-09-15 14:50:05 +0600

    custom list view data shown

--- app/src/main/java/com/example/asifkhan/customlistview/activities/MainActivity.java
+++ app/src/main/java/com/example/asifkhan/customlistview/activities/MainActivity.java
@@ -3,15 +3,56 @@ package com.example.asifkhan.customlistview.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 
 import com.example.asifkhan.customlistview.R;
+import com.example.asifkhan.customlistview.adapters.CustomListAdapter;
+import com.example.asifkhan.customlistview.models.UserInfo;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<UserInfo> userInfos;
+    private CustomListAdapter customListAdapter;
+    private ListView customListView;
+    private String[] names={
+            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan",
+            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan",
+            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan",
+            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan",
+            "Asif Khan","Tanvir Ahmed","Nafis Iqbal","Rahim Islam","Abir Hasan"
+    };
+    private String[] professions={
+            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst",
+            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst",
+            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst",
+            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst",
+            "Android Developer","Web Developer","Python Programmer","iOS Programmer","Data Analyst"
+    };
+    private int[] photos={
+            R.drawable.sample_5,R.drawable.sample_1,R.drawable.sample_6,R.drawable.sample_5,R.drawable.sample_5,
+            R.drawable.sample_0,R.drawable.sample_2,R.drawable.sample_3,R.drawable.sample_5,R.drawable.sample_2,
+            R.drawable.sample_6,R.drawable.sample_1,R.drawable.sample_4,R.drawable.sample_6,R.drawable.sample_4,
+            R.drawable.sample_0,R.drawable.sample_3,R.drawable.sample_3,R.drawable.sample_5,R.drawable.sample_4,
+            R.drawable.sample_7,R.drawable.sample_1,R.drawable.sample_5,R.drawable.sample_5,R.drawable.sample_0
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar((Toolbar)findViewById(R.id.toolbar));
+        customListView=(ListView)findViewById(R.id.custom_list_view);
+        userInfos=new ArrayList<>();
+        customListAdapter=new CustomListAdapter(userInfos,this);
+        customListView.setAdapter(customListAdapter);
+        getDatas();
+    }
+
+    // getting all the datas
+    private void getDatas(){
+        for(int count=0;count<names.length;count++){
+            userInfos.add(new UserInfo(names[count],professions[count],photos[count]));
+        }
     }
 }

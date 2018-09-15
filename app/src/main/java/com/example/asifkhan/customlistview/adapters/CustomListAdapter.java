907525e00dc1a8dd339453ed74aa8f79ec5aafda
Asif Khan <asifkhan.github@gmail.com>
2018-09-15 15:33:37 +0600

    option menu shown as popup menu

--- app/src/main/java/com/example/asifkhan/customlistview/adapters/CustomListAdapter.java
+++ app/src/main/java/com/example/asifkhan/customlistview/adapters/CustomListAdapter.java
@@ -2,18 +2,21 @@ package com.example.asifkhan.customlistview.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.asifkhan.customlistview.R;
 import com.example.asifkhan.customlistview.models.UserInfo;
 
 import java.util.ArrayList;
 
-public class CustomListAdapter extends BaseAdapter {
+public class CustomListAdapter extends BaseAdapter implements View.OnClickListener{
     private ArrayList<UserInfo> userInfos;
     private Context context;
 
@@ -41,17 +44,50 @@ public class CustomListAdapter extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater layoutInflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view=layoutInflater.inflate(R.layout.custom_list_view_layout,null);
-        ImageView photo;
+        ImageView photo,option;
         if(view==null){
             photo=new ImageView(context);
         }
         UserInfo userInfo=userInfos.get(i);
         photo=(ImageView)view.findViewById(R.id.photo);
+        option=(ImageView)view.findViewById(R.id.option);
         TextView name=(TextView)view.findViewById(R.id.name);
         TextView profession=(TextView)view.findViewById(R.id.profession);
         photo.setImageResource(userInfo.getPhoto());
         name.setText(userInfo.getName());
         profession.setText(userInfo.getProfession());
+        option.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.option:
+                showPopupMenu(view);
+                break;
+        }
+    }
+
+    // getting the popup menu
+    private void showPopupMenu(View view){
+        PopupMenu popupMenu=new PopupMenu(context,view);
+        popupMenu.getMenuInflater().inflate(R.menu.option_menu,popupMenu.getMenu());
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.edit:
+                        Toast.makeText(context, "Edit !", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.remove:
+                        Toast.makeText(context, "Remove !", Toast.LENGTH_SHORT).show();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
